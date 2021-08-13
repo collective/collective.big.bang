05624c08e70ec5b1b9bd10f0c79257adaa860332
Benoit Suttor <bsuttor@imio.be>
2021-08-13 15:55:23 +0200

    fix: collective.bigbang package name is not a package name authorized by pypi

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 
 setup(
-    name="collective.bigbang",
+    name="collective.big.bang",
     version="1.0a1",
     description="Add-on which create Plone site into Zope",
     long_description=long_description,
