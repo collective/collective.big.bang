7c32fb9c7ca05f65deb7947b4ed79bae4c764495
Godefroid Chapelle <gotcha@bubblenet.be>
2022-05-12 15:39:23 +0200

    backward compat

--- src/collective/big/bang/__init__.py
+++ src/collective/big/bang/__init__.py
@@ -2,5 +2,10 @@
 """Init and utils."""
 from zope.i18nmessageid import MessageFactory
 
+import sys
+from collective.big.bang import interfaces
+
+sys.modules['collective.big.bang.interface'] = interfaces
+
 
 _ = MessageFactory("collective.big.bang")
