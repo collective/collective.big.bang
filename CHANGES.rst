4177fb707d584ccc1abf8745b74deb15184515a1
odelaere <odelaere@users.noreply.github.com>
2022-05-13 12:07:48 +0200

    Preparing release 1.0.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.0.2 (unreleased)
+1.0.2 (2022-05-13)
 ------------------
 
 - Fix mountpoints break bid.bang execution.
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.big.bang",
-    version="1.0.2.dev0",
+    version="1.0.2",
     description="Add-on which create Plone site into Zope",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
