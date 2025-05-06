7f89f26862209537ee8277d35d992eee7587ecfb
Rémi Dubois <remi.dubois@gmail.com>
2025-05-06 10:16:44 +0200

    feat: remove z3c.jbot dependency

--- setup.py
+++ setup.py
@@ -55,10 +55,9 @@ setup(
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
-        "z3c.jbot",
         "plone.api>=1.8.4",
-        "plone.restapi",
         "plone.app.dexterity",
+        "plone.restapi",
     ],
     extras_require={
         "test": [
